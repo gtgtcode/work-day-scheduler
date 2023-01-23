b3aeaf667fc7c15479304361ef7d99571b605b91
GeorgeWorkPC <glthomas526@gmail.com>
2023-01-23 13:02:53 -0500

    added hour sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
 import { Button, ButtonGroup, Card } from '@mui/material';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ControlPanel from './components/ControlPanel';
+import TimeDisplay from './components/TimeDisplay';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main className="App">
+      <div className='grid grid-cols-4'>
+        <ControlPanel />
+        <TimeDisplay />
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/ControlPanel.js
@@ -0,0 +1,7 @@
+export default function ControlPanel() {
+    return (
+      <section className="p-5 h-screen border-r border-slate-700 text-center bg-zinc-900 mt-0">
+        <h1>Work Day Planner</h1>
+      </section>
+    );
+}
